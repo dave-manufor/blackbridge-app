649291e49d3b8c8a56362f82305819e443e8c489
dave-manufor <davemanufor@gmail.com>
2025-09-05 16:29:29 +0300

    Chore: Changed Env names

--- backend/src/config/bucket.config.ts
+++ backend/src/config/bucket.config.ts
@@ -8,9 +8,9 @@ export default {
   /**AWS secret access key */
   SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   /**Maximum bytes per multipart part */
-  MAX_PART_SIZE: Number(process.env.MAX_FILE_SIZE) || 15 * 1024 * 1024,
+  MAX_PART_SIZE: Number(process.env.MAX_PART_SIZE) || 15 * 1024 * 1024,
   /**Maximum bytes per block */
-  MAX_BLOCK_SIZE: Number(process.env.MAX_UPLOAD_SIZE) || 100 * 1024 * 1024,
+  MAX_BLOCK_SIZE: Number(process.env.MAX_BLOCK_SIZE) || 100 * 1024 * 1024,
   presignedUrl: {
     download: {
       /**Download presigned URL expiry in seconds */
