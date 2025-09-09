f2b763a02e58e903c452fd3ca48b16ec6d39b847
dave-manufor <davemanufor@gmail.com>
2025-09-09 09:36:02 +0300

    Fix: Change wrong env name

--- backend/src/services/notifications/config.ts
+++ backend/src/services/notifications/config.ts
@@ -1,7 +1,7 @@
 const notificationConfig = {
   DEFAULT_FROM_EMAIL: (process.env.DEFAULT_FROM_EMAIL as string) || 'no-reply@example.com',
   SUPPORT_EMAIL: (process.env.SUPPORT_EMAIL as string) || 'support@example.com',
-  BASE_URL: (process.env.BASE_URL as string) || 'https://localhost:5174',
+  BASE_URL: (process.env.BASE_APP_URL as string) || 'https://localhost:5174',
   RESEND_API_KEY: (process.env.RESEND_API_KEY as string) || 'your-default-api-key',
 };
 
