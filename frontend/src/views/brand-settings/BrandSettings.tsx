c6c2e6cc4110073667be2d8782a4ab316391079d
dave-manufor <davemanufor@gmail.com>
2025-09-14 04:03:15 +0300

    Fix: Set app header

--- frontend/src/views/brand-settings/BrandSettings.tsx
+++ frontend/src/views/brand-settings/BrandSettings.tsx
@@ -1,6 +1,12 @@
 import ComingSoonState from "@/components/ui/ComingSoonState";
+import useAppHeader from "@/hooks/context/useAppHeader";
+import { useEffect } from "react";
 
 const BrandSettings = () => {
+  const { setHeaderTitle } = useAppHeader();
+  useEffect(() => {
+    setHeaderTitle("Brand Settings");
+  }, [setHeaderTitle]);
   return <ComingSoonState />;
 };
 
--- frontend/src/views/file-requests/FileRequests.tsx
+++ frontend/src/views/file-requests/FileRequests.tsx
@@ -1,6 +1,12 @@
 import ComingSoonState from "@/components/ui/ComingSoonState";
+import useAppHeader from "@/hooks/context/useAppHeader";
+import { useEffect } from "react";
 
 const FileRequests = () => {
+  const { setHeaderTitle } = useAppHeader();
+  useEffect(() => {
+    setHeaderTitle("File Requests");
+  }, [setHeaderTitle]);
   return <ComingSoonState />;
 };
 
