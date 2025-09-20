37c6a7662fd50bc69cd5833b1dfa36f5542deceb
dave-manufor <davemanufor@gmail.com>
2025-09-20 13:24:36 +0300

    Task: Add outputs for REST API id & stage

--- terraform/production/api_gateway/outputs.tf
+++ terraform/production/api_gateway/outputs.tf
@@ -2,3 +2,13 @@ output "api_gateway_invoke_url" {
   description = "The base URL to invoke the API Gateway."
   value       = "https://${aws_api_gateway_rest_api.blackbridge_production_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.production.stage_name}"
 }
+
+output "api_gateway_id" {
+  description = "The API Gateway REST API id."
+  value       = aws_api_gateway_rest_api.blackbridge_production_api.id
+}
+
+output "stage_name" {
+  description = "API Gateway stage name."
+  value       = aws_api_gateway_stage.production.stage_name
+}
