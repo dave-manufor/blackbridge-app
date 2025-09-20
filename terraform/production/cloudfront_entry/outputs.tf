d3f361b68b5e441cabdd2e4d6545c3438b206995
dave-manufor <davemanufor@gmail.com>
2025-09-20 19:27:49 +0300

    Task: Redeploy app cdn

--- terraform/production/cloudfront_entry/outputs.tf
+++ terraform/production/cloudfront_entry/outputs.tf
@@ -1,4 +1,4 @@
-# output "app_cloudfront_domain" {
-#   description = "The domain name of the CloudFront distribution for the app (React + /api -> API Gateway)."
-#   value       = aws_cloudfront_distribution.app_distribution.domain_name
-# }
+output "app_cloudfront_domain" {
+  description = "The domain name of the CloudFront distribution for the app (React + /api -> API Gateway)."
+  value       = aws_cloudfront_distribution.app_distribution.domain_name
+}
