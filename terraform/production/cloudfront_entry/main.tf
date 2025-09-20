98609538ef86e5204f5a51a58f963c44df09cc13
dave-manufor <davemanufor@gmail.com>
2025-09-20 17:51:19 +0300

    Fix: Use Lamdba@Edge for SPA rewrites

--- terraform/production/cloudfront_entry/main.tf
+++ terraform/production/cloudfront_entry/main.tf
@@ -1,3 +1,53 @@
+# Lambda@Edge function to rewrite SPA routes to index.html
+locals {
+  spa_rewrite_js = templatefile("${path.module}/spa-rewrite.js.tpl", {
+    s3_origin_id = var.static_site_origin_id
+  })
+}
+
+data "archive_file" "spa_rewrite_zip" {
+  type        = "zip"
+  output_path = "${path.module}/lambda/spa-rewrite.zip"
+
+  source {
+    content  = local.spa_rewrite_js
+    filename = "index.js"
+  }
+}
+
+resource "aws_iam_role" "lambda_edge_role" {
+  name = "${var.app_name}-lambda-edge-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = [
+            "lambda.amazonaws.com",
+            "edgelambda.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "spa_rewrite" {
+  function_name = "${var.app_name}-spa-rewrite"
+  role          = aws_iam_role.lambda_edge_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  filename         = data.archive_file.spa_rewrite_zip.output_path
+  source_code_hash = data.archive_file.spa_rewrite_zip.output_base64sha256
+
+  publish = true
+}
+
+
 # CloudFront distribution that routes /api/* -> API Gateway, default -> React S3
 resource "aws_cloudfront_origin_access_control" "react_oac" {
   name                              = "${var.app_name}-react-oac"
@@ -43,6 +93,13 @@ resource "aws_cloudfront_distribution" "app_distribution" {
         forward = "none"
       }
     }
+
+    lambda_function_association {
+    event_type   = "origin-request"
+    lambda_arn   = aws_lambda_function.spa_rewrite.qualified_arn
+    include_body = false
+  }
+
   }
 
   ordered_cache_behavior {
@@ -79,18 +136,6 @@ resource "aws_cloudfront_distribution" "app_distribution" {
       }
     }
   }
-
-  custom_error_response {
-  error_code         = 404
-  response_code      = 200
-  response_page_path = "/index.html"
-}
-
-custom_error_response {
-  error_code         = 403
-  response_code      = 200
-  response_page_path = "/index.html"
-}
 
   restrictions {
     geo_restriction {
