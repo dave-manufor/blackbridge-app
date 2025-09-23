e04e89307256bc2d3ce785f485e8efd304649fb3
dave-manufor <davemanufor@gmail.com>
2025-09-23 14:36:12 +0300

    Fix: Fix cdn private key formatting if present

--- backend/src/config/bucket.config.ts
+++ backend/src/config/bucket.config.ts
@@ -16,7 +16,7 @@ export default {
   /**Cloudfront key pair id */
   AWS_CLOUDFRONT_KEY_PAIR_ID: process.env.AWS_CLOUDFRONT_KEY_PAIR_ID,
   /**Cloudfront private key */
-  STORAGE_CDN_PRIVATE_KEY: process.env.STORAGE_CDN_PRIVATE_KEY,
+  STORAGE_CDN_PRIVATE_KEY: process.env.STORAGE_CDN_PRIVATE_KEY ? process.env.STORAGE_CDN_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
   /**Presigned URL configurations */
   presignedUrl: {
     download: {
